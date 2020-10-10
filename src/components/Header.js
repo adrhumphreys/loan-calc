d9155dd2ee801d47bf32a323e99ea08ddd5abbcc
Adrian Humphreys <adrian.humphreys@silverstripe.com>
2020-10-10 19:44:49 +1300

    Fix emoji

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,10 @@ export default () => (
   <nav className="navbar navbar-light bg-light">
     <div className="container-fluid">
       <span className="navbar-brand mb-0 h1">
-        👩‍🌾&nbsp;&nbsp;Loan calculators
+        <span role="img" aria-label="It's a farmer">
+          👩‍🌾
+        </span>
+        &nbsp;&nbsp;Loan calculators
       </span>
     </div>
   </nav>
